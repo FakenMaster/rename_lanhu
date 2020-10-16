593db1e84e3b6e8d077cdae785a58775b0605fb6
linchenpeng <cplin@86links.com>
2020-10-16 18:37:18 +0800

    已经可以使用

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,17 +1,16 @@
 import 'dart:io';
 
-import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:filepicker_windows/filepicker_windows.dart';
 import 'package:flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
 import 'package:filepicker_windows/filepicker_windows.dart'
     as filepicker_windows;
-import 'package:giffy_dialog/giffy_dialog.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 import 'package:rxdart/rxdart.dart';
 import 'package:stringx/stringx.dart';
-import 'package:file_chooser/file_chooser.dart';
 import 'package:time/time.dart';
+import 'package:path/path.dart' as package_path;
 
 class HomePage extends StatefulWidget {
   HomePage({Key key, this.title}) : super(key: key);
@@ -26,6 +25,7 @@ class _HomePageState extends State<HomePage> {
   BehaviorSubject<String> sourcePath;
   BehaviorSubject<String> destinationPath;
   BehaviorSubject<bool> renameSubject;
+  PublishSubject<bool> loading;
 
   TextEditingController sourcePathController;
   TextEditingController destinationPathController;
@@ -35,6 +35,7 @@ class _HomePageState extends State<HomePage> {
     super.initState();
     sourcePath = BehaviorSubject();
     destinationPath = BehaviorSubject();
+    loading = PublishSubject();
 
     sourcePathController = TextEditingController()
       ..addListener(() {
@@ -53,6 +54,7 @@ class _HomePageState extends State<HomePage> {
 
   @override
   void dispose() {
+    loading.close();
     sourcePath.close();
     destinationPath.close();
     renameSubject.close();
@@ -63,130 +65,145 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            StreamBuilder<String>(
-                stream: sourcePath,
-                builder: (context, snapshot) {
-                  return Row(
-                    children: [
-                      Text(
-                        '来源路径：',
-                        style: DefaultTextStyle.of(context).style,
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Flexible(
-                        child: TextField(
-                          controller: sourcePathController,
+    return StreamBuilder<bool>(
+        stream: loading,
+        builder: (context, snapshot) {
+          return ModalProgressHUD(
+            inAsyncCall: snapshot.data == true,
+            dismissible: false,
+            child: Scaffold(
+              appBar: AppBar(
+                title: Text(widget.title),
+              ),
+              body: Stack(
+                children: [
+                  Center(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        StreamBuilder<String>(
+                            stream: sourcePath,
+                            builder: (context, snapshot) {
+                              return Row(
+                                children: [
+                                  Text(
+                                    '来源路径：',
+                                    style: DefaultTextStyle.of(context).style,
+                                  ),
+                                  SizedBox(
+                                    width: 10,
+                                  ),
+                                  Flexible(
+                                    child: TextField(
+                                      controller: sourcePathController,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 10,
+                                  ),
+                                  RaisedButton.icon(
+                                    onPressed: () async {
+                                      String path = await filePicker();
+                                      if (path.notNullAndEmpty()) {
+                                        sourcePathController.text = path;
+                                        sourcePath.add(path);
+                                      }
+                                    },
+                                    icon: Icon(Icons.more_horiz),
+                                    label: Text('选择文件夹'),
+                                  )
+                                ],
+                              );
+                            }),
+                        SizedBox(
+                          height: 40,
                         ),
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      RaisedButton.icon(
-                        onPressed: () async {
-                          String path = await filePicker();
-                          if (path.notNullAndEmpty()) {
-                            sourcePathController.text = path;
-                            sourcePath.add(path);
-                          }
-                        },
-                        icon: Icon(Icons.more_horiz),
-                        label: Text('选择文件夹'),
-                      )
-                    ],
-                  );
-                }),
-            SizedBox(
-              height: 40,
-            ),
-            StreamBuilder<String>(
-                stream: destinationPath,
-                builder: (context, snapshot) {
-                  return Row(
-                    children: [
-                      Text(
-                        '目标路径：',
-                        style: DefaultTextStyle.of(context).style,
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Flexible(
-                        child: TextField(
-                          controller: destinationPathController,
+                        StreamBuilder<String>(
+                            stream: destinationPath,
+                            builder: (context, snapshot) {
+                              return Row(
+                                children: [
+                                  Text(
+                                    '目标路径：',
+                                    style: DefaultTextStyle.of(context).style,
+                                  ),
+                                  SizedBox(
+                                    width: 10,
+                                  ),
+                                  Flexible(
+                                    child: TextField(
+                                      controller: destinationPathController,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 10,
+                                  ),
+                                  RaisedButton.icon(
+                                    onPressed: () async {
+                                      String path = await filePicker();
+                                      if (path.notNullAndEmpty()) {
+                                        destinationPathController.text = path;
+                                        destinationPath.add(path);
+                                      }
+                                    },
+                                    icon: Icon(Icons.more_horiz),
+                                    label: Text('选择文件夹'),
+                                  )
+                                ],
+                              );
+                            }),
+                        SizedBox(
+                          height: 40,
                         ),
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      RaisedButton.icon(
-                        onPressed: () async {
-                          String path = await filePicker();
-                          if (path.notNullAndEmpty()) {
-                            destinationPathController.text = path;
-                            destinationPath.add(path);
-                          }
-                        },
-                        icon: Icon(Icons.more_horiz),
-                        label: Text('选择文件夹'),
-                      )
-                    ],
-                  );
-                }),
-            SizedBox(
-              height: 40,
-            ),
-            StreamBuilder<bool>(
-                stream: renameSubject,
-                builder: (context, snapshot) {
-                  return Center(
-                    child: RaisedButton(
-                      onPressed: snapshot.data == true
-                          ? () {
-                              if (checkValidDirectory('来源', sourcePath.value) &&
-                                  checkValidDirectory(
-                                      '目标', destinationPath.value)) {
-                                /// 重命名、分配到目标文件夹
-                                showDialog(
-                                    context: context,
-                                    builder: (_) => AlertDialog(
-                                          title: Text('确定开始转换吗?'),
-                                          actions: [
-                                            FlatButton(
-                                              onPressed: () =>
-                                                  Navigator.pop(context),
-                                              child: Text('取消'),
-                                            ),
-                                            FlatButton(
-                                              onPressed: () {
-                                                Navigator.pop(context);
-                                                renameBlueLakeFiles();
-                                              },
-                                              child: Text('确定'),
-                                            ),
-                                          ],
-                                        ));
-                              }
-                            }
-                          : null,
-                      child: Text('重命名、分配到相应文件夹'),
+                        StreamBuilder<bool>(
+                            stream: renameSubject,
+                            builder: (context, snapshot) {
+                              return Center(
+                                child: RaisedButton(
+                                  onPressed: snapshot.data == true
+                                      ? () {
+                                          if (checkValidDirectory(
+                                                  '来源', sourcePath.value) &&
+                                              checkValidDirectory('目标',
+                                                  destinationPath.value)) {
+                                            /// 重命名、分配到目标文件夹
+                                            showDialog(
+                                                context: context,
+                                                builder: (_) => AlertDialog(
+                                                      title: Text('确定开始转换吗?'),
+                                                      actions: [
+                                                        FlatButton(
+                                                          onPressed: () =>
+                                                              Navigator.pop(
+                                                                  context),
+                                                          child: Text('取消'),
+                                                        ),
+                                                        FlatButton(
+                                                          onPressed: () {
+                                                            Navigator.pop(
+                                                                context);
+                                                            renameBlueLakeFiles();
+                                                          },
+                                                          child: Text('确定'),
+                                                        ),
+                                                      ],
+                                                    ));
+                                          }
+                                        }
+                                      : null,
+                                  child: Text('重命名、分配到相应文件夹'),
+                                ),
+                              );
+                            }),
+                      ],
                     ),
-                  );
-                }),
-          ],
-        ),
-      ),
-    );
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
   }
 
   bool checkValidDirectory(String prefix, String path) {
@@ -196,14 +213,14 @@ class _HomePageState extends State<HomePage> {
       Flushbar(
         message: '${prefix}文件夹不存在',
         duration: 3.seconds,
-      ).show(context);
+      ).show(this.context);
       return false;
     }
     if (fileSystemEntityType != FileSystemEntityType.directory) {
       Flushbar(
         message: '请选择${prefix}文件夹',
         duration: 3.seconds,
-      ).show(context);
+      ).show(this.context);
       return false;
     }
     return true;
@@ -218,7 +235,48 @@ class _HomePageState extends State<HomePage> {
     return null;
   }
 
-  renameBlueLakeFiles() async {}
+  renameBlueLakeFiles() async {
+    loading.add(true);
+    Directory sourceDirectory = Directory(sourcePath.value);
+    await for (FileSystemEntity entity in sourceDirectory.list()) {
+      String path = entity.path;
+      if (!FileSystemEntity.isFileSync(entity.path)) {
+        return;
+      }
+
+      // String dirName = package_path.dirname(path);
+      String extension = package_path.extension(path);
+      String nameWithoutExtension = package_path.basenameWithoutExtension(path);
+      String fileName = nameWithoutExtension;
+      String folderName;
+      if (nameWithoutExtension.endsWith('@2x')) {
+        folderName = '2.0x';
+        fileName = nameWithoutExtension.substring(
+            0, nameWithoutExtension.lastIndexOf('@2x'));
+      } else if (nameWithoutExtension.endsWith('@3x')) {
+        folderName = '3.0x';
+        fileName = nameWithoutExtension.substring(
+            0, nameWithoutExtension.lastIndexOf('@3x'));
+      }
+
+      String separator = package_path.separator;
+      String destinationFilePath = destinationPath.value +
+          '$separator' +
+          (folderName != null ? folderName + "$separator" : '') +
+          fileName +
+          extension;
+
+      File file = File(destinationFilePath);
+      if (file.existsSync()) file.deleteSync();
+
+      file.createSync(recursive: true);
+    }
+    loading.add(false);
+    Flushbar(
+      message: '转换成功',
+      duration: 3.seconds,
+    ).show(context);
+  }
 
   Future<String> filePickerWindows() async {
     OpenFilePicker openFilePicker = filepicker_windows.OpenFilePicker();
